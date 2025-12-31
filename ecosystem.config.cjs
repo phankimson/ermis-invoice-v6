d107303bc0a06358d13ef6dcbad5190fa16210cb
Son Phan <phankimson1988@gmail.com>
2025-12-31 20:23:46 +0700

    upp

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ermis-invoice',
-      script: 'bin/server.js',
+      script: 'bin/server.ts',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
