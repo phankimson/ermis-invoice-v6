93b0dc660e8af6bd8c081a89b71c291738d8c104
Son Phan <phankimson1988@gmail.com>
2025-12-31 19:23:46 +0700

    upp

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'web-app',
-      script: './build/server.js', // The entry file after building
-      instances: 'max',             // Run maximum instances for load balancing
-      exec_mode: 'cluster',         // Enable cluster mode
-      autorestart: true,            // Automatically restart if it crashes
-      // Optional: Set the ENV_PATH to a secure location for your .env file
-      // env_production: {
-      //   ENV_PATH: '/etc/secrets/.env'
-      // }
+      script: './server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
     },
   ],
-};
+}
